80a7daa0ff018f49e85397f8e71a197737a15f46
evelinmus <82497847+evelinmus@users.noreply.github.com>
2021-04-14 12:48:53 +0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,220 @@
+var loadedData = [];
+
+function loadBookEditItem() {
+    localStorage = window.localStorage;
+    editItem = JSON.parse(localStorage.getItem("editItem"));
+    console.log(editItem);
+    document.getElementById("_id").innerHTML = editItem["_id"];
+    document.getElementById("title").value = editItem["title"];
+    document.getElementById("fullname").value = editItem["fullname"];   
+    document.getElementById("author").value = editItem["author"];   
+    document.getElementById("numberofpages").value = editItem["numberofpages"];
+    document.getElementById("price").value = editItem["price"];
+    document.getElementById("currency").value = editItem["currency"];
+    document.getElementById("language").value = editItem["language"];
+    document.getElementById("edition").value = editItem["edition"];
+    document.getElementById("publisher").value = editItem["publisher"];
+    document.getElementById("publishingdate").value = editItem["publishingdate"];
+    document.getElementById("originalpubdate").value = editItem["originalpubdate"];
+    document.getElementById("genre").value = editItem["genre"];
+}
+function loadPaintingEditItem() {
+    localStorage = window.localStorage;
+    editItem = JSON.parse(localStorage.getItem("editItem"));
+    console.log(editItem);
+    document.getElementById("_id").value = editItem["_id"];
+    document.getElementById("fullname").value = editItem["fullname"];
+    document.getElementById("lvl").value= editData["lvl"];
+    document.getElementById("style").value = editItem["style"];
+    document.getElementById("color").value = editItem["color"];
+    document.getElementById("favpainter").value = editData["favpainter"];
+    document.getElementById("painttype").value = editData["painttype"];
+    document.getElementById("favartmedium").value = editData["favartmedium"];
+    document.getElementById("inspo").value = editData["inspo"];
+    document.getElementById("favpainting").value = ["favpainting"];
+
+    }
+
+function editData(id) {
+    var tmp = id.split("edit_");
+    var item_id = tmp[1];
+
+    loadedData.forEach(item => {
+        if ( item._id == item_id) {
+            console.log(item); 
+            localStorage = window.localStorage;
+            localStorage.setItem('PaintingeditItem', JSON.stringify(item));
+            if (item["project"] == "Painting") {
+            document.location  = "Painting.html"; 
+            } else {
+            document.location  = "Painting.html"; 
+            }
+        }
+    })
+}
+
+function deleteData(id) {
+
+    var r = confirm("Are you sure you want to delete the item with the following ID? " + id);
+    if (r == false) {
+        return;
+    }
+
+    var tmp = {
+        "id": id
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: "https://cse120-2021-api.herokuapp.com/data/delete",
+        data: tmp,
+        cache: false,
+        dataType : 'json',
+        success: function (data) {
+            console.log("success");
+            document.getElementById("div" + id).style.display = "none";
+        },
+        error: function (xhr) {
+            console.error("Error in post", xhr);
+        },
+        complete: function () {
+            console.log("Complete");  
+        }
+    });
+}
+
+function saveData() {
+	var tmp = {
+		"test": "Data"
+	}
+
+  $.ajax({
+      type: 'POST',
+      url: "https://cse120-2021-api.herokuapp.com/data",
+      data: tmp,
+      cache: false,
+      dataType : 'json',
+      success: function (data) {
+        console.log("success");
+      },
+      error: function (xhr) {
+          console.error("Error in post", xhr);
+      },
+      complete: function () {
+          console.log("Complete");  
+      }
+  });
+}
+
+function loadExistingData() {
+  myPaintingData = [];
+  myBookData = [];
+  otherData = [];
+  $.ajax({
+      type : "GET",
+      url : "https://cse120-2021-api.herokuapp.com/data",
+      dataType : "json",
+      success : function(data) {
+        loadedData = data.data;
+        data.data.forEach(elem => {
+          if (elem["owner"] == "Evelina Musaelyan") {
+            if (elem["project"] == "Painting") {
+              myPaintingData.push(elem);
+            } else {
+              myBookData.push(elem);
+            }
+          } else {
+            otherData.push(elem);
+          }
+        })
+        displayData(myPaintingData, "PaintingDataContainer");
+        displayData(myBookData, "bookDataContainer");
+        displayData(otherData, "otherDataContainer");
+      },
+      error : function(data) {
+          console.log("Error")
+      }
+  });
+}
+
+function displayData(data, containerDivName) {
+  document.getElementById(containerDivName).innerHTML = "";
+  data.forEach(elem => {
+    var item = document.createElement("div");
+    item.id = "div" + elem["_id"];
+    item.className = "item";
+    if (Object.keys(elem).length == 1) {
+      var span = document.createElement("span");
+      span.innerHTML = "<i>Empty Element with autogenerated ID: </i>" + elem["_id"];
+      item.appendChild(span);
+    }
+    Object.keys(elem).forEach(key => {
+      if (key != "_id") {
+        var span = document.createElement("span");
+
+        var b = document.createElement("b");
+        b.innerHTML = key + ": ";
+        span.appendChild(b);
+        
+        span.className = "item";
+        if (elem[key]) {
+            span.innerHTML += elem[key];
+        } else {
+            var span1 = document.createElement("span");
+            span1.className = "undefined";
+            span1.innerHTML = "N/A";
+            span.appendChild(span1)
+        }
+        item.appendChild(span);
+
+        var br = document.createElement("br");
+        item.appendChild(br);
+      }
+    })
+    var edit_button = document.createElement("button");
+    edit_button.innerHTML = "Edit";
+    edit_button.id = "edit_" + elem["_id"];
+    edit_button.className = "edit";
+    edit_button.addEventListener("click", function(e){
+        editData(e.target.id);
+    }, false);
+    item.appendChild(edit_button);
+
+    var button = document.createElement("button");
+    button.innerHTML = "Delete";
+    button.id = elem["_id"];
+    button.addEventListener("click", function(e){
+        deleteData(e.target.id);
+    }, false);
+    item.appendChild(button);
+    document.getElementById(containerDivName).appendChild(item);
+  })
+
+}
+
+function toggleOtherData() {
+  var otherData = document.getElementById("otherDataContainer");
+  if (otherData.style.display == "block") {
+    otherData.style.display = "none";
+  } else {
+    otherData.style.display = "block";
+  }
+}
+
+function togglePaintingData() {
+  var PaintingData = document.getElementById("PaintingDataContainer");
+  if (PaintingData.style.display == "block") {
+    PaintingData.style.display = "none";
+  } else {
+    PaintingData.style.display = "block";
+  }
+}
+
+function toggleBookData() {
+  var bookData = document.getElementById("bookDataContainer");
+  if (bookData.style.display == "block") {
+    bookData.style.display = "none";
+  } else {
+    bookData.style.display = "block";
+  }
+}
